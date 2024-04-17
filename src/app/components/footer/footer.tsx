9a8b787cf952b0c4019530aa2ba60ab8ad6d2b0d
Daniel Riley <dann.riley@icloud.com>
2024-04-17 18:41:01 +0200

    added dynamic date to the footer

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,9 +1,11 @@
 import styles from "./footer.module.css";
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={styles.footer}>
       <p className={styles.p}>Made with 🖤 in Berlin</p>
+      <p className={styles.p}>{currentYear}</p>
     </footer>
   );
 }
